e21c751eb79cbe87c905900aaab2cd86d454684a
Paul Brubaker <pbrubak@gmail.com>
2021-08-13 12:30:56 -0700

    add active styling to NavLinks, change default data type for pokeDetail state

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,42 @@
 import { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+const classNames = require('classnames');
 
 class Header extends Component {
+    state = {
+        home: true,
+        pokemon: false
+    }
+
+    whichPage = (e, page, bool) => {
+        this.setState({ [page]: bool })
+    }
+
     render () {
         return (
             <header>
                 <h1>Pokedex</h1>
                 <div className="links">
-                    <NavLink to="/" >Home</NavLink>
-                    <NavLink to="/pokemon">Pokemon</NavLink>
+                    <NavLink 
+                        to="/" 
+                        className={classNames({ 'is-current-page': this.state.home })}
+                        onClick={
+                            (e) => {
+                                this.whichPage(e, 'home', true);
+                                this.whichPage(e, 'pokemon', false);
+                            }
+                        }
+                    >Home</NavLink>
+                    <NavLink
+                        to="/pokemon"
+                        className={classNames({ 'is-current-page': this.state.pokemon})}
+                        onClick={
+                            (e) => {
+                                this.whichPage(e, 'home', false);
+                                this.whichPage(e, 'pokemon', true);
+                            }
+                        }
+                    >Pokemon</NavLink>
                 </div>
             </header>
         )
